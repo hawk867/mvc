8a40b3a2287f267f3363f1fc26fd96cd9d91561b
Daniel Esteban Cepeda Rojas <whisper1831@hotmail.com>
2026-03-24 20:02:29 -0500

    Se agrega lombok al proyecto

--- src/main/java/org/danielesteban/task/model/Task.java
+++ src/main/java/org/danielesteban/task/model/Task.java
@@ -1,7 +1,12 @@
 package org.danielesteban.task.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import java.util.Objects;
 
+@Setter
+@Getter
 public class Task {
     private String id;
     private String title;
@@ -12,38 +17,6 @@ public class Task {
         this.id = id;
         this.title = title;
         this.description = description;
-        this.completed = completed;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public Boolean getCompleted() {
-        return completed;
-    }
-
-    public void setCompleted(Boolean completed) {
         this.completed = completed;
     }
 
